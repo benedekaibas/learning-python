05f35f9034c1f13d3af56f597392b91fbde02793
kaibas01 <kaibas01@allegheny.edu>
2023-11-28 17:11:12 -0500

    finished code

--- string_finder.py
+++ string_finder.py
@@ -10,10 +10,7 @@ count = 0
 for search in word:
     if search == letter_looking_for:
         count += 1
+
+
+print(count)
         
-
-        #print(count.index()) #check how should this work!
-    #else:
-        #print("Code not working well!")
-
-

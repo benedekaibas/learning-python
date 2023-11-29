0e906f89ccc44f6e9b3fe4aa93b3ee5c0e18f89e
kaibas01 <kaibas01@allegheny.edu>
2023-11-29 10:40:38 -0500

    added file

---
+++ ANSI_C_függvény.py
@@ -0,0 +1,25 @@
+"""
+Készítsen egy olyan szabványos ANSI C függvényt, amely visszatérési értékül egy double szám n-edik byte-ja medik bitjét szolgáltatja. 
+Bemenő paraméterek: maga a double típusú valós szám, valamint n és m értéke, amelyek 1 és 8
+közötti egészek. 
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

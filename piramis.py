53d37c90cbfbd99eb74136f7d3b576ad854643fa
kaibas01 <82393336+benedekaibas@users.noreply.github.com>
2023-12-05 13:48:26 -0500

    Add files via upload

---
+++ piramis.py
@@ -0,0 +1,34 @@
+
+global ROWS
+global LINES
+ROWS = 10
+
+def piramis():
+    for i in range(ROWS):
+        for j in range(i + 1):
+            print("*", end = "")
+        print("\n")
+    for i in range(ROWS):
+        for j in range(ROWS - i ):
+            print("*", end = "")
+        print("\n")
+    
+
+
+
+if __name__ == "__main__":
+    piramis()
+
+
+
+
+
+
+
+
+
+
+
+
+
+

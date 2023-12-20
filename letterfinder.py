a11318a2877d16622a52ce5113b3a2bfcd58f64d
kaibas01 <82393336+benedekaibas@users.noreply.github.com>
2023-12-20 15:18:04 -0500

    Create letterfinder.py

---
+++ letterfinder.py
@@ -0,0 +1,32 @@
+"""
+Írjon programot, ami beolvas egy szöveget, majd egy betűt, és megmondja, a betű hányszor szerepel a szövegben.
+
+"""
+
+
+def letter_finder():
+    text = str(input('Enter a text: '))
+    letter = str(input('Enter a letter: '))
+
+    if letter in text:
+        to_lower = letter.lower()
+        count = to_lower.count(letter)
+        print(f"We have found {count} {letter} letters in your text!")
+
+
+if __name__ == "__main__":
+    letter_finder()
+
+
+
+
+
+
+
+
+
+
+
+
+
+

ab7189448526b50f471fe0a9fe9b8361e805438a
kaibas01 <82393336+benedekaibas@users.noreply.github.com>
2023-12-22 07:20:24 -0500

    final code

--- letterfinder.py
+++ letterfinder.py
@@ -3,30 +3,16 @@
 
 """
 
-# TODO: Make the code cleaner in the if statement
 def letter_finder():
     text = str(input('Enter a text: '))
     letter = str(input('Enter a letter: '))
 
     if letter in text:
-        to_lower = letter.lower()
+        to_lower = text.lower()
         count = to_lower.count(letter)
         print(f"We have found {count} {letter} letters in your text!")
 
 
 if __name__ == "__main__":
     letter_finder()
-
-
-
-
-
-
-
-
-
-
-
-
-
 

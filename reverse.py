e4d225b8154cd9fd435ccf7caac930df6ab7d8f9
kaibas01 <82393336+benedekaibas@users.noreply.github.com>
2023-12-25 05:50:14 -0500

    added code

--- reverse.py
+++ reverse.py
@@ -1,1 +1,21 @@
+"""
+Example 1:
 
+Input: x = 123
+Output: 321
+
+Example 2:
+
+Input: x = -123
+Output: -321
+"""
+
+def reverse():
+    x = int(input('Enter number: '))
+
+    to_list = [x]
+
+    return to_list
+
+if __name__ == "__main__":
+    reverse()

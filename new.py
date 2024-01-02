dd7e25868e0efa3ef3dade33e2f7e6c2a2e6f75d
kaibas01 <kaibas01@allegheny.edu>
2024-01-02 09:00:00 -0500

    added pandas plus code

--- new.py
+++ new.py
@@ -1,1 +1,12 @@
 #C:\Users\kaiba\OneDrive\Asztali gép\learning-python\new.py
+
+import pandas as pd
+
+def main():
+    filename = "Hungary-streets.xls"
+
+    pd.read_excel(filename, index_col= 0)
+
+
+if __name__ == "__main__":
+    main()

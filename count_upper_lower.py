164590ab036e7a09261550d7364d0913cf52147d
kaibas01 <kaibas01@allegheny.edu>
2024-01-05 09:12:49 -0500

    started code

--- count_upper_lower.py
+++ count_upper_lower.py
@@ -1,13 +1,12 @@
-#C:\Users\kaiba\OneDrive\Asztali gép\learning-python\new.py
 
-import pandas as pd
-
-def main():
-    filename = "Hungary-streets.xls"
-
-    pd.read_excel(filename, index_col= 0)
+def count():
+    string = "Hello World!"
 
     
 
+
+
 if __name__ == "__main__":
-    main()
+    count()
+
+

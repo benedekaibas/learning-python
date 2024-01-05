0e08919dc610624e68329721ff850d2482ad6dd6
kaibas01 <kaibas01@allegheny.edu>
2024-01-05 09:24:33 -0500

    added the description of the task

--- count_upper_lower.py
+++ count_upper_lower.py
@@ -1,9 +1,15 @@
+"""
+Write a program that accepts a sentence and calculate the number of upper case letters and lower case letters. 
+Suppose the following input is supplied to the program: Hello world! Then, the output should be: UPPER CASE 1 LOWER CASE 9
+"""
+
 
 def count():
     string = "Hello World!"
-
+    uppercase_alphabet = "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
+    count = 0
     
-
+    
 
 
 if __name__ == "__main__":

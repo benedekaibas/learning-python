366415465e279f42d60de93680a60d3b5e9cbadf
kaibas01 <kaibas01@allegheny.edu>
2024-01-05 09:29:44 -0500

    done with code

--- count_upper_lower.py
+++ count_upper_lower.py
@@ -8,9 +8,12 @@ def count():
     string = "Hello World!"
     uppercase_alphabet = "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
     count = 0
-    
-    
 
+    for letters in string:
+        if letters in uppercase_alphabet:
+            count += 1
+    print(f"There are {count} uppercase letters in {string}")
+        
 
 if __name__ == "__main__":
     count()

2a6ac35cde9224ab04f45872477245c5714b4580
kaibas01 <kaibas01@allegheny.edu>
2024-01-06 15:17:31 -0500

    added file and done with code

---
+++ alphabet.py
@@ -0,0 +1,19 @@
+"""
+Írj egy Python programot, amely megmondja előfordul-e (igen/nem) a Debrecen szó a temp.txt
+fájlban!
+"""
+
+#this file opens the temp.txt file since it is required based on the readme
+
+filename = "temp.txt"
+count = 0
+with open(filename, "r") as fh:
+    content = fh.read()
+    lowerText = content.lower()
+    if "debrecen" in lowerText:
+        count += 1
+    print(count)
+
+
+
+
